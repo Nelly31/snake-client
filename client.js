f762f97237b8be60c929cea528e911fa95a0fba9
Nelly31 <nelly_main@hotmail.com>
2019-07-03 20:07:05 +0000

    setting up userInterface

--- client.js
+++ client.js
@@ -18,8 +18,22 @@ const connect = function() {
   });
   return conn;
 };
+ 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
 
-
+  //allows users to exit with ctrl c
+  const handleUserInput =
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
 
 
 
