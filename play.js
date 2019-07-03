3c5dc94a15efb2d06d5034a83077f54354f495af
Nelly31 <nelly_main@hotmail.com>
2019-07-03 18:11:10 +0000

    updated

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
-const net = require('net');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.0.102',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  return conn;
-};
-
-
+const {connect} = require('./client');
 
 console.log('Connecting....');
+
 connect();
